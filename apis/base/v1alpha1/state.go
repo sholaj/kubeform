62bd4057a78cdbc71cb1e057b51e97e1b084f679
Tamal Saha <tamal@appscode.com>
2019-10-22 01:02:59 -0700

    Update state.go

--- apis/base/v1alpha1/state.go
+++ apis/base/v1alpha1/state.go
@@ -1,5 +1,7 @@
 package v1alpha1
 
+// xref: https://github.com/hashicorp/terraform/blob/d7bce857cf7e2ab8cd6378196ee95b9a935acb5c/states/statefile/version4.go#L497
+
 // +k8s:openapi-gen=true
 // +k8s:deepcopy-gen:interfaces
 type State struct {
